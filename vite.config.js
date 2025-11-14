cbc1336ec0d39a8026e35c3d5f9ea67e7eb613a6
dilshad <hmydilshadkt@gmail.com>
2025-11-14 15:08:50 +0530

    chore: add cross-env dependency and update package scripts for Electron build configuration

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const isElectronBuild = process.env.BUILD_TARGET === "electron";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: isElectronBuild ? "./" : "/",
   plugins: [react(), tailwindcss()],
 });
